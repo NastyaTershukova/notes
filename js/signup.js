4006f5db5400d7a1d259ea97f4888eabbee0ed41
devio <64607085+devio10@users.noreply.github.com>
2024-06-09 09:28:49 +0400

    updated sign up frontend

---
+++ js/signup.js
@@ -0,0 +1,108 @@
+currentStep = 0;
+
+function nextStep() {
+    showWarning(false);
+    document.querySelector(`#step${currentStep}`).classList.add('done');
+    currentStep++;
+    document.querySelector(`#step${currentStep}`).classList.remove('hidden');
+    document.querySelector(`#step${currentStep}`).classList.remove('done');
+}
+
+function previousStep() {
+    showWarning(false);
+    if (currentStep == 0) {
+        window.open('landing.html', '_self');
+        return;
+    }
+    document.querySelector(`#step${currentStep}`).classList.add('hidden');
+    currentStep--;
+    document.querySelector(`#step${currentStep}`).classList.remove('done');
+}
+
+function setStep(value) {
+    document.querySelector(`#step${currentStep}`).classList.add('hidden');
+    currentStep = value;
+    document.querySelector(`#step${currentStep}`).classList.remove('done');
+    document.querySelector(`#step${currentStep}`).classList.remove('hidden');
+}
+
+function isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    return emailRegex.test(email)
+}
+
+function showWarning(value) {
+    if (!value) {
+        document.querySelector('.warning').classList.remove('shown');
+        return;
+    }
+    document.querySelector('.warning').classList.add('shown');
+    document.querySelector('.warning').innerHTML = `<i class="ph-warning"></i>${value}`;
+}
+
+function checkAllData() {
+    let email = document.querySelector(`#email`).value;
+    let password = document.querySelector(`#password`).value;
+    let first_name = document.querySelector(`#name`).value;
+    let last_name = document.querySelector(`#last_name`).value;
+
+    if (!isValidEmail(email)) {
+        setStep(0);
+        showWarning('Введите правильный адрес электронной почты.');
+        return;
+    }
+    if (password.length < 6) {
+        setStep(0);
+        showWarning('Слишком короткий пароль. Минимальная длина: 6 символов.');
+        return;
+    }
+    if (first_name.trim().length == 0) {
+        setStep(1);
+        showWarning('Введите ваше имя.');
+        return;
+    }
+    if (last_name.trim().length == 0) {
+        setStep(1);
+        showWarning('Введите вашу фамилию.');
+        return;
+    }
+
+    signUp();
+}
+
+async function signUp() {
+    try {
+
+        document.querySelector('.popup_buttons').children[0].innerHTML = `<div class="spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>`;
+        document.querySelector('.popup_buttons').classList.add('disabled');
+        document.querySelector('#picture__input').disabled = true;
+    
+        const response = await fetch('php/upload_image.php', {
+          method: 'POST',
+          body: JSON.stringify({
+            email: email,
+            password: password,
+            name: first_name,
+            last_name: last_name,
+            image: loadedImage
+          })
+        })
+        const result = await response.json();
+        // console.log(result);
+    
+        if (result.success) {
+          // console.log(result.success);
+          imagePaths[lastCreatedImage] = result.success;
+          displayImage(document.getElementById(lastCreatedImage), result.success);
+    
+          newImagePopup(false);
+        } else if (result.error) {
+          console.log(result.error);
+          //TODO ВЫВОДИТЬ ОШИБКУ
+          pushToHistory();
+          newImagePopup(false);
+        }
+      } catch (error) {
+        console.error('Error: ', error)
+      }
+}
